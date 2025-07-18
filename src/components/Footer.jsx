db6c06339bd143f8ad35acb1ada75ddd985f8702
Santhosh <santhoshprogrammer07@gmail.com>
2025-07-18 16:56:32 +0530

    " Footer component updated"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -148,7 +148,7 @@ const Footer = () => {
                             variants={fadeIn('left', 1.0)}
                             className="text-gray-600 text-sm"
                         >
-                            Created by Md Al Mamun
+                            Created by Santhosh
                         </motion.p>
                     </motion.div>
                 </motion.div>
